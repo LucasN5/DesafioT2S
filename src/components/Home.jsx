0a6586f63cecf3440b1ccae802724b8c8a4e6bef
LucasN5 <lucasxben@hotmail.com>
2024-02-11 12:29:47 -0300

    Home text change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
       
         <h1 className="text-4xl font-bold  py-1 text-BenefitsColor">Tudo começa com <br/>planejamento e organização</h1>
 
-        <h3 className="py-5 tracking-widest  text-BenefitsColor">A T2S oferece o sistema de organização <br/>que seu time precisa</h3>
+        <h3 className="py-5 tracking-widest  text-BenefitsColor">A T2S tem o sistema de organização <br/>que seu time precisa</h3>
 
         <a href="https://www.t2s.com.br/home" target="_blank" rel="noreferrer">
           <button className="rounded-full bg-ProdutsColor text-BenefitsColor font-medium p-1 px-4">
